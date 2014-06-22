76135adfb14d5aa78dbcd52b31958f65c273ae2b
Gustavo Salvo <gsalvo@vasal.cl>
2014-06-22 01:31:16 -0400

    formularios faltantes

--- pingesoBL-web/src/main/java/managedBean/perinatalHistory/PerinatalHistory.java
+++ pingesoBL-web/src/main/java/managedBean/perinatalHistory/PerinatalHistory.java
@@ -19,6 +19,7 @@ import javax.faces.bean.ViewScoped;
 public class PerinatalHistory {
 
     String[] familyHistory = new String[5];
+    
     String[] personalHistory = new String[5];
     String reasonAbortion;
     String[] bornCheck;
@@ -36,8 +37,297 @@ public class PerinatalHistory {
     //embarazo actual
     int currentWeight;
     int usualWeight;
+    int size;
     Date FUR;
     Date FURO;
+    String[] estimated = new String[4];
+    String doubts;
+    Date FPBirth;
+    int gestationalAge;
+    int numberDays;
+    String doubtsHEA;
+    String reason;
+    String blood;
+    String bloodType;
+    String sensitized;
+    
+    
+    String examinationCN;
+    String examinationMN;
+    String examinationON;
+    String normalPelvis;
+    String normalPapanic;
+    String normalCervix;
+    String VIH;
+    
+    Date VDRL;
+    String VDRLOption;
+    Date VDRL2;
+    String VDRLOption2;
+    
+    String HCTOCheck;
+    double HTCTOFloat;
+    Date HCTODate;
+    
+    String smoker;
+    int cantCigars ;
+
+    public String getHCTOCheck() {
+        return HCTOCheck;
+    }
+
+    public void setHCTOCheck(String HCTOCheck) {
+        this.HCTOCheck = HCTOCheck;
+    }
+
+    public double getHTCTOFloat() {
+        return HTCTOFloat;
+    }
+
+    public void setHTCTOFloat(double HTCTOFloat) {
+        this.HTCTOFloat = HTCTOFloat;
+    }
+
+    public Date getHCTODate() {
+        return HCTODate;
+    }
+
+    public void setHCTODate(Date HCTODate) {
+        this.HCTODate = HCTODate;
+    }
+
+    public String getSmoker() {
+        return smoker;
+    }
+
+    public void setSmoker(String smoker) {
+        this.smoker = smoker;
+    }
+
+    public int getCantCigars() {
+        return cantCigars;
+    }
+
+    public void setCantCigars(int cantCigars) {
+        this.cantCigars = cantCigars;
+    }
+    
+    public Date getVDRL2() {
+        return VDRL2;
+    }
+
+    public void setVDRL2(Date VDRL2) {
+        this.VDRL2 = VDRL2;
+    }
+    public String getExaminationCN() {
+        return examinationCN;
+    }
+
+    public void setExaminationCN(String examinationCN) {
+        this.examinationCN = examinationCN;
+    }
+
+    public String getExaminationMN() {
+        return examinationMN;
+    }
+
+    public void setExaminationMN(String examinationMN) {
+        this.examinationMN = examinationMN;
+    }
+
+    public String getExaminationON() {
+        return examinationON;
+    }
+
+    public void setExaminationON(String examinationON) {
+        this.examinationON = examinationON;
+    }
+
+    public String getNormalPelvis() {
+        return normalPelvis;
+    }
+
+    public void setNormalPelvis(String normalPelvis) {
+        this.normalPelvis = normalPelvis;
+    }
+
+    public String getNormalPapanic() {
+        return normalPapanic;
+    }
+
+    public void setNormalPapanic(String normalPapanic) {
+        this.normalPapanic = normalPapanic;
+    }
+
+    public String getNormalCervix() {
+        return normalCervix;
+    }
+
+    public void setNormalCervix(String normalCervix) {
+        this.normalCervix = normalCervix;
+    }
+
+    public String getVIH() {
+        return VIH;
+    }
+
+    public void setVIH(String VIH) {
+        this.VIH = VIH;
+    }
+
+    public Date getVDRL() {
+        return VDRL;
+    }
+
+    public void setVDRL(Date VDRL) {
+        this.VDRL = VDRL;
+    }
+
+    public String getVDRLOption() {
+        return VDRLOption;
+    }
+
+    public void setVDRLOption(String VDRLOption) {
+        this.VDRLOption = VDRLOption;
+    }
+
+
+    public String getVDRLOption2() {
+        return VDRLOption2;
+    }
+
+    public void setVDRLOption2(String VDRLOption2) {
+        this.VDRLOption2 = VDRLOption2;
+    }
+    
+    
+    
+    
+    
+    
+
+    public String getBlood() {
+        return blood;
+    }
+
+    public void setBlood(String blood) {
+        this.blood = blood;
+    }
+
+    public String getBloodType() {
+        return bloodType;
+    }
+
+    public void setBloodType(String bloodType) {
+        this.bloodType = bloodType;
+    }
+
+    public String getSensitized() {
+        return sensitized;
+    }
+
+    public void setSensitized(String sensitized) {
+        this.sensitized = sensitized;
+    }
+    
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+   
+    
+    public int getCurrentWeight() {
+        return currentWeight;
+    }
+
+    public void setCurrentWeight(int currentWeight) {
+        this.currentWeight = currentWeight;
+    }
+
+    public int getUsualWeight() {
+        return usualWeight;
+    }
+
+    public void setUsualWeight(int usualWeight) {
+        this.usualWeight = usualWeight;
+    }
+
+    public Date getFUR() {
+        return FUR;
+    }
+
+    public void setFUR(Date FUR) {
+        this.FUR = FUR;
+    }
+
+    public Date getFURO() {
+        return FURO;
+    }
+
+    public void setFURO(Date FURO) {
+        this.FURO = FURO;
+    }
+
+    public String[] getEstimated() {
+        return estimated;
+    }
+
+    public void setEstimated(String[] estimated) {
+        this.estimated = estimated;
+    }
+
+    public String getDoubts() {
+        return doubts;
+    }
+
+    public void setDoubts(String doubts) {
+        this.doubts = doubts;
+    }
+
+    public Date getFPBirth() {
+        return FPBirth;
+    }
+
+    public void setFPBirth(Date FPBirth) {
+        this.FPBirth = FPBirth;
+    }
+
+    public int getGestationalAge() {
+        return gestationalAge;
+    }
+
+    public void setGestationalAge(int gestationalAge) {
+        this.gestationalAge = gestationalAge;
+    }
+
+    public int getNumberDays() {
+        return numberDays;
+    }
+
+    public void setNumberDays(int numberDays) {
+        this.numberDays = numberDays;
+    }
+
+    public String getDoubtsHEA() {
+        return doubtsHEA;
+    }
+
+    public void setDoubtsHEA(String doubtsHEA) {
+        this.doubtsHEA = doubtsHEA;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+    
+    
     
     
     public void test(){

2b5b8c544a00d37d8ca802ea3efc99ca349bb9bb
Gustavo Salvo <gsalvo@vasal.cl>
2014-06-23 02:52:58 -0400

    formularios faltantes

--- pingesoBL-web/src/main/java/managedBean/vitalSigns/AddVitalSigns.java
+++ pingesoBL-web/src/main/java/managedBean/vitalSigns/AddVitalSigns.java
@@ -62,20 +62,104 @@ public class AddVitalSigns {
     int max = 0;
 
     private int peso;
+    private String unitPeso;
+    private int maxPeso;
+    private int minPeso;
+
     private int altura;
+    private String unitAltura;
+    private int maxAltura;
+    private int minAltura;
+
     private int temperatura;
+    private String unitTemperatura;
+    private int maxTemperatura;
+    private int minTemperatura;
+
     private int saturacion;
+    private String unitSaturacion;
+    private int maxSaturacion;
+    private int minSaturacion;
+
     private int presionSistolica;
+    private String unitPresionSistolica;
+    private int maxPresionSistolica;
+    private int minPresionSistolica;
+
     private int presionDiastolica;
+    private String unitPresionDiastolica;
+    private int maxPresionDiastolica;
+    private int minPresionDiastolica;
+
+    private String GeneralUnit;
+    private int maxGeneralUnit;
+    private int minGeneralUnit;
+
+    private List<SignosVitales> controllerVitalSigns = new ArrayList<SignosVitales>();
+
+    public void returnUnit() {
+        for (int i = 0; i < searchVitalSigns.size(); i++) {
+
+            if (searchVitalSigns.get(i).getNombreSvital().equals("Peso")) {
+                unitPeso = searchVitalSigns.get(i).getUnidad();
+                minPeso = searchVitalSigns.get(i).getRangoMin();
+                maxPeso = searchVitalSigns.get(i).getRangoMax();
+            }
+            if (searchVitalSigns.get(i).getNombreSvital().equals("Altura")) {
+                unitAltura = searchVitalSigns.get(i).getUnidad();
+                minAltura = searchVitalSigns.get(i).getRangoMin();
+                maxAltura = searchVitalSigns.get(i).getRangoMax();
+            }
+            if (searchVitalSigns.get(i).getNombreSvital().equals("Temperatura")) {
+                unitTemperatura = searchVitalSigns.get(i).getUnidad();
+                minTemperatura = searchVitalSigns.get(i).getRangoMin();
+                maxTemperatura = searchVitalSigns.get(i).getRangoMax();
+            }
+            if (searchVitalSigns.get(i).getNombreSvital().equals("Saturación O2")) {
+                unitSaturacion = searchVitalSigns.get(i).getUnidad();
+                minSaturacion = searchVitalSigns.get(i).getRangoMin();
+                maxSaturacion = searchVitalSigns.get(i).getRangoMax();
+            }
+            if (searchVitalSigns.get(i).getNombreSvital().equals("Presión Sistólica")) {
+                unitPresionSistolica = searchVitalSigns.get(i).getUnidad();
+                minPresionSistolica = searchVitalSigns.get(i).getRangoMin();
+                maxPresionSistolica = searchVitalSigns.get(i).getRangoMax();
+            }
+            if (searchVitalSigns.get(i).getNombreSvital().equals("Presión Diastólica")) {
+                unitPresionDiastolica = searchVitalSigns.get(i).getUnidad();
+                minPresionDiastolica = searchVitalSigns.get(i).getRangoMin();
+                maxPresionDiastolica = searchVitalSigns.get(i).getRangoMax();
+            }
+
+        }
+    }
+
+    public void returnGeneralUnit() {
+        if (vitalSignsId == 0) {
+            GeneralUnit = "";
+            maxGeneralUnit=0;
+            minGeneralUnit=0;            
+        } else {
+            for (int i = 0; i < searchVitalSigns.size(); i++) {
+                if (searchVitalSigns.get(i).getIdSvitales() == vitalSignsId) {
+                    GeneralUnit = searchVitalSigns.get(i).getUnidad();
+                    maxGeneralUnit = searchVitalSigns.get(i).getRangoMax();
+                    minGeneralUnit = searchVitalSigns.get(i).getRangoMin();
+                }
+            }
+        }
+
+    }
 
     @PostConstruct
     public void init() {
-        String[] vitalSignsO = {"Peso","Altura","Temperatura","Saturación O2",
-        "Presión Sistólica", "Presión Diastólica"};
+        String[] vitalSignsO = {"Peso", "Altura", "Temperatura", "Saturación O2",
+            "Presión Sistólica", "Presión Diastólica"};
         searchVitalSigns = vitalSignsFacade.findAll();
-        for(int i=0; i<searchVitalSigns.size(); i++){
-            for(int j = 0; j< vitalSignsO.length;j++){
-                if(searchVitalSigns.get(i).getNombreSvital().equals(vitalSignsO[j])){
+        returnUnit();
+        for (int i = 0; i < searchVitalSigns.size(); i++) {
+            for (int j = 0; j < vitalSignsO.length; j++) {
+                if (searchVitalSigns.get(i).getNombreSvital().equals(vitalSignsO[j])) {
                     searchVitalSigns.remove(i);
                     i--;
                     break;
@@ -111,7 +195,7 @@ public class AddVitalSigns {
             }
             max++;
         }
-        
+
         Date fecha = new Date();
 
         selectedVitalSign = vitalSignsFacade.searchByName("Peso");
@@ -123,7 +207,6 @@ public class AddVitalSigns {
         newMuesta.setGrupo(max);
         createSamplesAlways.add(newMuesta);
 
-        
         newMuesta = new Muesta(null);
         selectedVitalSign = vitalSignsFacade.searchByName("Altura");
         newMuesta.setFecha(fecha);
@@ -169,7 +252,6 @@ public class AddVitalSigns {
         newMuesta.setGrupo(max);
         createSamplesAlways.add(newMuesta);
 
-
         for (int i = 0; i < createSamplesAlways.size(); i++) {
             if (createSamplesAlways.get(i).getValor() != 0) {
                 sampleFacade.create(createSamplesAlways.get(i));
@@ -181,8 +263,21 @@ public class AddVitalSigns {
         for (Muesta createSample : createSamples) {
             sampleFacade.create(createSample);
         }
-        
+
         resetVitalSigns();
+    }
+    
+    public void deleteVitalSignal(Muesta deleteSample){
+        System.out.println(deleteSample.getIdMuestra());
+        System.out.println(createSamples.get(0).getIdMuestra());
+//        for(int i = 0; i< createSamples.size();i++){
+//            System.out.println(createSamples.get(i).getIdMuestra());
+//            System.out.println(deleteSample.getIdMuestra());
+//            if(deleteSample.getIdMuestra() == createSamples.get(i).getIdMuestra()){
+//                createSamples.remove(deleteSample);
+//                return;
+//            }
+//        }
     }
 
     public void addVitalSignsPatients() {
@@ -212,7 +307,7 @@ public class AddVitalSigns {
         createSamples.add(newMuesta);
     }
 
-    public void resetVitalSigns(){
+    public void resetVitalSigns() {
         createSamples.clear();
         max = 0;
         peso = 0;
@@ -225,7 +320,7 @@ public class AddVitalSigns {
         vitalSignsId = 0;
         vitalSignsValue = 0;
     }
-    
+
     public int getPeso() {
         return peso;
     }
@@ -304,6 +399,174 @@ public class AddVitalSigns {
 
     public void setCreateSamples(List<Muesta> createSamples) {
         this.createSamples = createSamples;
+    }
+
+    public String getUnitPeso() {
+        return unitPeso;
+    }
+
+    public void setUnitPeso(String unitPeso) {
+        this.unitPeso = unitPeso;
+    }
+
+    public String getUnitAltura() {
+        return unitAltura;
+    }
+
+    public void setUnitAltura(String unitAltura) {
+        this.unitAltura = unitAltura;
+    }
+
+    public String getUnitTemperatura() {
+        return unitTemperatura;
+    }
+
+    public void setUnitTemperatura(String unitTemperatura) {
+        this.unitTemperatura = unitTemperatura;
+    }
+
+    public String getUnitSaturacion() {
+        return unitSaturacion;
+    }
+
+    public void setUnitSaturacion(String unitSaturacion) {
+        this.unitSaturacion = unitSaturacion;
+    }
+
+    public String getUnitPresionSistolica() {
+        return unitPresionSistolica;
+    }
+
+    public void setUnitPresionSistolica(String unitPresionSistolica) {
+        this.unitPresionSistolica = unitPresionSistolica;
+    }
+
+    public String getUnitPresionDiastolica() {
+        return unitPresionDiastolica;
+    }
+
+    public void setUnitPresionDiastolica(String unitPresionDiastolica) {
+        this.unitPresionDiastolica = unitPresionDiastolica;
+    }
+
+    public String getGeneralUnit() {
+        return GeneralUnit;
+    }
+
+    public void setGeneralUnit(String GeneralUnit) {
+        this.GeneralUnit = GeneralUnit;
+    }
+
+    public int getMaxPeso() {
+        return maxPeso;
+    }
+
+    public void setMaxPeso(int maxPeso) {
+        this.maxPeso = maxPeso;
+    }
+
+    public int getMinPeso() {
+        return minPeso;
+    }
+
+    public void setMinPeso(int minPeso) {
+        this.minPeso = minPeso;
+    }
+
+    public int getMaxAltura() {
+        return maxAltura;
+    }
+
+    public void setMaxAltura(int maxAltura) {
+        this.maxAltura = maxAltura;
+    }
+
+    public int getMinAltura() {
+        return minAltura;
+    }
+
+    public void setMinAltura(int minAltura) {
+        this.minAltura = minAltura;
+    }
+
+    public int getMaxTemperatura() {
+        return maxTemperatura;
+    }
+
+    public void setMaxTemperatura(int maxTemperatura) {
+        this.maxTemperatura = maxTemperatura;
+    }
+
+    public int getMinTemperatura() {
+        return minTemperatura;
+    }
+
+    public void setMinTemperatura(int minTemperatura) {
+        this.minTemperatura = minTemperatura;
+    }
+
+    public int getMaxSaturacion() {
+        return maxSaturacion;
+    }
+
+    public void setMaxSaturacion(int maxSaturacion) {
+        this.maxSaturacion = maxSaturacion;
+    }
+
+    public int getMinSaturacion() {
+        return minSaturacion;
+    }
+
+    public void setMinSaturacion(int minSaturacion) {
+        this.minSaturacion = minSaturacion;
+    }
+
+    public int getMaxPresionSistolica() {
+        return maxPresionSistolica;
+    }
+
+    public void setMaxPresionSistolica(int maxPresionSistolica) {
+        this.maxPresionSistolica = maxPresionSistolica;
+    }
+
+    public int getMinPresionSistolica() {
+        return minPresionSistolica;
+    }
+
+    public void setMinPresionSistolica(int minPresionSistolica) {
+        this.minPresionSistolica = minPresionSistolica;
+    }
+
+    public int getMaxPresionDiastolica() {
+        return maxPresionDiastolica;
+    }
+
+    public void setMaxPresionDiastolica(int maxPresionDiastolica) {
+        this.maxPresionDiastolica = maxPresionDiastolica;
+    }
+
+    public int getMinPresionDiastolica() {
+        return minPresionDiastolica;
+    }
+
+    public void setMinPresionDiastolica(int minPresionDiastolica) {
+        this.minPresionDiastolica = minPresionDiastolica;
+    }
+
+    public int getMaxGeneralUnit() {
+        return maxGeneralUnit;
+    }
+
+    public void setMaxGeneralUnit(int maxGeneralUnit) {
+        this.maxGeneralUnit = maxGeneralUnit;
+    }
+
+    public int getMinGeneralUnit() {
+        return minGeneralUnit;
+    }
+
+    public void setMinGeneralUnit(int minGeneralUnit) {
+        this.minGeneralUnit = minGeneralUnit;
     }
 
 }
